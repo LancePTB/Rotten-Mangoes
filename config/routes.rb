6833a6f5910e258fc05d654839bd04e830c63450
Lance Barlaan <LPTBarlaan@gmail.com>
2015-09-29 20:33:39 +0000

    Added admin controller namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,18 @@ RottenMangoes::Application.routes.draw do
   get "users/create"
   resources :movies
   root to: 'movies#index'
-  
+   
+  namespace :admin do
+    resources :articles, :comments
+  end
+   
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
