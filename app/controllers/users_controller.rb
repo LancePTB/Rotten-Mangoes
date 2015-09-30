26ceb21c3f4637a1177f8a8c83ffcffca6f670e6
Lance Barlaan <LPTBarlaan@gmail.com>
2015-09-30 04:22:45 +0000

    email notification functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to "/admin/users"
+    UserMailer.notify_email(@user)
+    redirect_to("/admin/users", notice: 'User was successfully deleted.')
+   
   end
 
   protected
